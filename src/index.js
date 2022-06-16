0b1cd040534ab63e41fa4ea2fcb6dd9b6b63f2d7
Koima-Sam <sam17koima@gmail.com>
2022-06-16 14:53:07 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let task = document.getElementById('new-task-description').value;
+    let newTask  = document.createElement('li');
+    newTask.appendChild(document.createTextNode(task))
+    document.getElementById('tasks').appendChild(newTask);
+  })
 });
